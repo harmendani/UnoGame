b349f4f779a731cd89469730ec192484be44adbd
Pedro Harmendani <pedrohnh@hotmail.com>
2018-05-01 19:04:40 -0300

    Adds .gitignore file

---
+++ MotorVisão.h
@@ -0,0 +1,38 @@
+/* Visão dos jogos são contagens, predições e verificações em relação as cartas e
+** são utilizados para que o motor de IA do jogo tome as melhores decisões.
+*/
+typedef struct VisaoContagem
+{
+    int i_quantMesa;
+    int i_quantMontante;
+
+} st_Contagem;
+
+typedef struct VisaoMesa
+{
+    carta c_cartaMesa;
+
+} st_Mesa;
+
+typedef struct VisaoJogador
+{
+    int i_quantJogador;
+    int i_quantadversario;
+
+} st_Jogador;
+
+typedef struct VisaoTotal
+{
+    st_Contagem contagem;
+    st_Mesa mesa;
+    st_Jogador jogador;
+} vision;
+
+/* Procedimentos para a visão Contagem */
+int v_QuantMesa();
+int v_QuantMontante();
+/* Procedimentos para a visão Mesa */
+carta v_VerificarCartaMesa();
+/* Procedimento para a visão Jogador */
+int v_QuantJogador();
+int v_QuantAdversario();
