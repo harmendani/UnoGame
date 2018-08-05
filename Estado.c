712db140a967792614400dc6e2003be809986a04
Pedro Harmendani <pedrohnh@hotmail.com>
2018-08-05 14:31:17 -0300

    Add rotinas sbes_1 ao 3 - recebe &player como arg

--- Estado.c
+++ Estado.c
@@ -1,6 +1,6 @@
 /* Implementa as rotinas de 'Estado.h' */
 
-static void start_StateGame(estado *s)
+void start_StateGame(estado *s)
 {
     /*Inicializa sub-estados S*/
     strcpy(s->state_contagem, DEFAULT);
@@ -23,12 +23,12 @@ char *build_StateGame()
     return DEFAULT;
 }
 
-static char *build_SubEstado_1()
+static char *build_SubEstado_1(player *j)
 {
-    char sub_estado_1[4];        // Guarda s1
-    strcpy(sub_estado_1, "000"); // Inicializa valor Default
-    int montante = v_QuantMontante();
-    int pilhaMesa = v_QuantMesa();
+    char *sub_estado_1 = (char *)malloc(4 * sizeof(char)); // Guarda s1
+
+    int montante = j->visaoPlayer.contagem.i_quantMontante;
+    int pilhaMesa = j->visaoPlayer.contagem.i_quantMesa;
     if (montante <= pilhaMesa)
     {
         strcpy(sub_estado_1, S1);
@@ -48,17 +48,60 @@ static char *build_SubEstado_1()
     return sub_estado_1;
 }
 
-static char *build_SubEstado_2()
+static char *build_SubEstado_2(player *j)
 {
 
-    char sub_estado_2[4]; // Guarda s2
-    strcpy(sub_estado_2, "000");
+    char *sub_estado_2 = (char *)malloc(4 * sizeof(char)); // Guarda s2
+
+    int contCartaAcao, contCartaCoringa4, contCartaCoringa;
+
+    contCartaCoringa4 = contadorDeCartasCuringa_4(M_l);
+    contCartaAcao = contadorDeCartasAcao(M_l);
+    contCartaCoringa = contadorDeCartasCuringa(M_l);
+    if (j->visaoPlayer.contagem.i_quantMesa > 3)
+    {
+        if (contCartaCoringa4 == QTD_CARTAS_CORINGA || contCartaAcao >= 12)
+        {
+            strcpy(sub_estado_2, S4);
+        }
+        else
+        {
+            if (contCartaCoringa >= 4 || contCartaAcao >= 6)
+            {
+                strcpy(sub_estado_2, S5);
+            }
+            else
+            {
+                strcpy(sub_estado_2, S6);
+            }
+        }
+    }
+    else
+    {
+        strcpy(sub_estado_2, S6);
+    }
+
     return sub_estado_2;
 }
 
-static char *build_SubEstado_3()
+static char *build_SubEstado_3(player *j)
 {
-    char sub_estado_3[4];        //Guarda s3
-    strcpy(sub_estado_3, "000"); // Inicializa valor Default
+    char *sub_estado_3 = (char *)malloc(5 * sizeof(char)); //Guarda s3
+    int quantJogador = j->visaoPlayer.jogador.i_quantJogador;
+    int quantAdv = j->visaoPlayer.jogador.i_quantadversario;
+
+    if (quantJogador > quantAdv)
+    {
+        strcpy(sub_estado_3, S7);
+    }
+    if (quantAdv == quantJogador)
+    {
+        strcpy(sub_estado_3, S8);
+    }
+    else
+    {
+        strcpy(sub_estado_3, S9);
+    }
+
     return sub_estado_3;
 }
