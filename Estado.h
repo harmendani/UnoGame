dc9286c52171a6d6426eefab3ebc82764c5db5b3
Pedro Harmendani <pedrohnh@hotmail.com>
2018-08-09 20:51:24 -0300

    Struct Estado - correção para tamanho 11 de statGame e stateAnt.

--- Estado.h
+++ Estado.h
@@ -1,10 +1,8 @@
 /* Define e manipula estados do jogo */
 
-
 //CONSTANTES
 /* Define os estados do jogo para cada vis�o*/
-//Valor default - inicial
-#define DEFAULT "000000000"
+
 //V1 (Baixo, Médio e Alto) - Visão Contagem
 #define S1 "001"
 #define S2 "010"
@@ -22,15 +20,10 @@
 
 struct Estado
 {
-    /* Salva os subestados atuais*/
-    char state_contagem[4];
-    char state_mesa[4];
-    char state_maos[4];
-
     /* Salva o estado atual do jogo  */
-    char stateGame[10];
-    /* Salva o pr�ximo estado do jogo ap�s executar uma a��o */
-    char stateNext[10];
+    char stateGame[11];
+    /* Salva o estado anterior do jogo ap�s executar uma a��o */
+    char stateAnt[11];
 };
 typedef struct Estado estado;
 
@@ -44,22 +37,19 @@ typedef struct Estado estado;
 /*  Ao realizar uma a��o, salva stateNext[n] em stateGame[n].
 *   Depois salva S em stateNext[n] - próximo estado do jogo.
 */
-static void save_StateGame();
+static void save_StateAnd();
 
-/*Define NULL para inicializar todos as Strings da struct Estado */
-void start_StateGame(estado *);
-
-/* Calcula o estado S corrente para o jogador atual a partir de (s1, s2, s3) */
-char *build_StateGame();
+/* Calcula o estado S atual para o jogador corrente a partir de (s1, s2, s3) */
+void build_StateGame(player *);
 
 /* Calcula e devolve sub-estado s1 */
-static char *build_SubEstado_1(player*);
+static char *build_SubEstado_1(player *);
 
 /* Calcula e devolve sub-estado s2 */
-static char *build_SubEstado_2(player*);
+static char *build_SubEstado_2(player *);
 
 /* Calcula e devolve sub-estado s3 */
-static char *build_SubEstado_3(player*);
+static char *build_SubEstado_3(player *);
 
 #ifndef ESTADO_C
 #define ESTADO_C
