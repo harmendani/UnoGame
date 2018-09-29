adc846e17cb7331a482505efd0ca79f94c524cb9
Pedro Harmendani <pedrohnh@hotmail.com>
2018-09-29 20:27:32 -0300

    Add contador de cartas por cor e numero no motorVisao

--- MotorVisao.c
+++ MotorVisao.c
@@ -8,19 +8,27 @@ static void inicializarMotorVisao(player *p)
     // Visão Mesa
     p->visaoPlayer.mesa.c_cartaMesa = NULL;
 
+    p->visaoPlayer.mesa.historico.i_amarelo = 0;
+    p->visaoPlayer.mesa.historico.i_azul = 0;
+    p->visaoPlayer.mesa.historico.i_verde = 0;
+    p->visaoPlayer.mesa.historico.i_vermelho = 0;
+
     //Visão Jogador
     p->visaoPlayer.jogador.i_quantJogador = 0;
     p->visaoPlayer.jogador.i_quantadversario = 0;
 
-    //Visão Histórico
-    p->visaoPlayer.historico.i_amarelo = 0;
-    p->visaoPlayer.historico.i_azul = 0;
-    p->visaoPlayer.historico.i_verde = 0;
-    p->visaoPlayer.historico.i_vermelho = 0;
+    p->visaoPlayer.jogador.historico.i_amarelo = 0;
+    p->visaoPlayer.jogador.historico.i_azul = 0;
+    p->visaoPlayer.jogador.historico.i_verde = 0;
+    p->visaoPlayer.jogador.historico.i_vermelho = 0;
+
+    //Visão Mesa / Jogador - Fator número da carta
+   
 
     for (int i = 0; i < 10; i++)
     {
-        p->visaoPlayer.historico.numero[i] = 0;
+        p->visaoPlayer.mesa.historico.numero[i] = 0;
+        p->visaoPlayer.jogador.historico.numero[i] = 0;
     }
 }
 
@@ -48,8 +56,7 @@ int v_QuantPlayer(player *jogador)
 void executarMotorVisao(player *p)
 {
     inicializarMotorVisao(p);       // Zera variáveis antes de computar as visões do player* p
-    contadorDeCartasPorTipo(p);     // Conta e salva as cartas por tipo das mãos do jogador atual.
-    contadorDeCartasporCor(p, M_l); // Salva o histórico por cor da mesa
+    contadorDeCartasPorTipo(p);     // Conta e salva as cartas por tipo das mãos do jogador atual.     
     build_StateGame(p);             // Salva o estado atual do jogador antes da jogada
 
     // Visão Contagem
@@ -58,8 +65,12 @@ void executarMotorVisao(player *p)
 
     // Visão Mesa
     p->visaoPlayer.mesa.c_cartaMesa = v_VerificarCartaMesa();
+    contadorDeCartasporCorMesa(p, M_l);
+    contadorDeCartasPorNumeroMesa(p, M_l);
 
     //Visão Jogador
     p->visaoPlayer.jogador.i_quantJogador = v_QuantPlayer(p);
     p->visaoPlayer.jogador.i_quantadversario = v_QuantPlayer(p->adversario);
+    contadorDeCartasporCorJogador(p, p->listaMaos);
+    contadorDeCartasPorNumeroJogador(p, p->cartasNormal);
 }
